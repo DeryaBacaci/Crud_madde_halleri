d8ca0a525218436fe0444d9bf0351e40f417618d
DeryaBacaci <107432656+DeryaBacaci@users.noreply.github.com>
2023-01-19 12:19:50 +0300

    Proje dosyası ekle.

---
+++ Controllers/TablesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Crud_madde_halleri.Models;
+
+namespace Crud_madde_halleri.Controllers
+{
+    public class TablesController : Controller
+    {
+        private Database1Entities1 db = new Database1Entities1();
+
+        // GET: Tables
+        public ActionResult Index()
+        {
+            return View(db.Table.ToList());
+
+
+        }
+
+        // GET: Tables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Table.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // GET: Tables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tables/Create
+        // Aşırı gönderim saldırılarından korunmak için bağlamak istediğiniz belirli özellikleri etkinleştirin. 
+        // Daha fazla bilgi için bkz. https://go.microsoft.com/fwlink/?LinkId=317598.
+
+        private bool IsUnique(string adi)
+        {
+            var isExists = db.Table.Any(i => i.MaddeAd == adi);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaddeID,MaddeAd,Sıcaklık,ErimeNoktası,KaynamaNoktası,HalDurumu")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Table.Add(table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table);
+        }
+
+        // GET: Tables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Table.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Tables/Edit/5
+        // Aşırı gönderim saldırılarından korunmak için bağlamak istediğiniz belirli özellikleri etkinleştirin. 
+        // Daha fazla bilgi için bkz. https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaddeID,MaddeAd,Sıcaklık,ErimeNoktası,KaynamaNoktası,HalDurumu")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table);
+        }
+
+        // GET: Tables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Table.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Tables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table table = db.Table.Find(id);
+            db.Table.Remove(table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        //public ActionResult Hal(int id)
+        //{
+        //    var cisim = db.Table.Find(id);
+
+
+        //    if ( cisim.Sıcaklık < cisim.erimenoktasi)
+        //    {
+        //        return "katı";
+        //    }
+        //    else if (cisim.Sıcaklık > erimenoktasi && sicaklik < kaynamanoktası)
+        //    {
+        //        return "sıvı";
+        //    }
+        //    else
+        //    {
+        //        return "gaz";
+
+        //    }
+        }
+    }
+//}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Crud_madde_halleri.MvcApplication" Language="C#" %>
